1aa6ad2987746a76444b646b9074d2249895f7ef
brandon-emjones <56854875+brandon-emjones@users.noreply.github.com>
2023-04-24 11:09:21 -0400

    Finished calculator logic for whole number calculations

--- app/src/main/java/com/bj/coolcalculator/CalcActivity.java
+++ app/src/main/java/com/bj/coolcalculator/CalcActivity.java
@@ -7,8 +7,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CalcActivity extends AppCompatActivity {
+
+
+    TextView resultView;
+
+    public enum Operation {
+        ADD, SUBTRACT, DIVIDE, MULTIPLY, EQUAL
+    }
+
+    String runningNumber = "";
+    String leftValue = "";
+    String rightValue = "";
+    Operation currentOperation;
+    int result = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,123 +48,167 @@ public class CalcActivity extends AppCompatActivity {
         ImageButton divideBtn = findViewById(R.id.divideBtn);
         ImageButton calcBtn = findViewById(R.id.calcBtn);
 
-        TextView resultView = findViewById(R.id.resultsText);
+        resultView = findViewById(R.id.resultsText);
+        resultView.setText("0");
 
         oneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('1');
             }
         });
 
         twoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('2');
             }
         });
 
         threeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('3');
             }
         });
 
         fourBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('4');
             }
         });
 
         fiveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('5');
             }
         });
 
         sixBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('6');
             }
         });
 
         sevenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('7');
             }
         });
 
         eightBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('8');
             }
         });
 
         nineBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('9');
             }
         });
 
         zeroBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                numberPress('0');
             }
         });
 
         plusBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.ADD);
             }
         });
 
         minusBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.SUBTRACT);
             }
         });
 
         multiplyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.MULTIPLY);
             }
         });
 
         divideBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.DIVIDE);
             }
         });
 
         clearBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                leftValue = "";
+                rightValue = "";
+                result = 0;
+                runningNumber = "";
+                currentOperation = null;
+                resultView.setText("0");
             }
         });
 
         calcBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                processOperation(Operation.EQUAL);
             }
         });
+    }
 
+    void processOperation(Operation operation) {
+        if (currentOperation != null) {
 
+            if (runningNumber != "") {
+                rightValue = runningNumber;
+                runningNumber = "";
 
+                switch (currentOperation) {
+                    case ADD:
+                        result = Integer.parseInt(leftValue) + Integer.parseInt(rightValue);
+                        break;
+                    case SUBTRACT:
+                        result = Integer.parseInt(leftValue) - Integer.parseInt(rightValue);
+                        break;
+                    case MULTIPLY:
+                        result = Integer.parseInt(leftValue) * Integer.parseInt(rightValue);
+                        break;
+                    case DIVIDE:
+                        result = Integer.parseInt(leftValue) / Integer.parseInt(rightValue);
+                        break;
+                }
 
+                leftValue = String.valueOf(result);
+                resultView.setText(leftValue);
+
+                //Toast toast = Toast.makeText(this, "here", Toast.LENGTH_SHORT);
+
+            }
+
+        } else {
+            leftValue = runningNumber;
+            runningNumber = "";
+            //Toast toast = Toast.makeText(this, "here", Toast.LENGTH_SHORT);
+        }
+        currentOperation = operation;
+    }
+
+    void numberPress(char num) {
+        runningNumber += num;
+        resultView.setText(runningNumber);
     }
 
 
